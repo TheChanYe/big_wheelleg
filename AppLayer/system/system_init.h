bdeb4068fca6097eca6e76ffe29651971dcad8a2
TheChanYe <132055434+TheChanYe@users.noreply.github.com>
2026-08-29 10:20:15 +0800

    build(cpp): add C++ ABI guard to system init API

--- AppLayer/system/system_init.h
+++ AppLayer/system/system_init.h
@@ -9,11 +9,19 @@
 
 #include "main.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * 功能：完成业务任务启动前的基础初始化。
  * 参数：无。
  * 返回值：E_OK 表示成功，E_OUT_OF_MEMORY 表示互斥锁创建失败。
  */
 int System_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
